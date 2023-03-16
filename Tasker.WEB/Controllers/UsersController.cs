56343482ee2416ede34f51acb9d51ac28126c6ad
Litest <bolshov@inbox.ru>
2023-03-16 09:01:03 +0300

    Добавление View для пользователей

---
+++ Tasker.WEB/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tasker.WEB.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
